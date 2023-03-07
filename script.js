508f72f48276363d8d55d1adc8990c121a42a883
Daren Muhammad <darenmuhammad10@gmail.com>
2023-03-07 13:35:39 +0700

    hpus

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 console.log("fungsi pertama");
-console.log("fungsi kedua");
-console.log("fungsi ketiga");
-console.log("ini perubahan nyata");
-console.log("ini perubahan nyata");
 
 const add = (a,b) => a + b;
 console.log(add(1,2));
